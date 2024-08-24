a61d6b96ac40a95e4b46093da669b2d0f9386680
Alex Dumitrascu <alexdumitrascu06@gmail.com>
2024-08-24 18:34:19 +0300

    first commit

---
+++ Divergence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using TriggersTools.SteinsGate;
+
+static void make_divergence(string text)
+{
+    int margin = -9;
+    var divergenceArgs = new DivergenceArgs
+    {
+        Scale = DivergenceScale.Large,
+        Spacing = new DivergenceSpacing(margin, margin),
+        // Background = Color.FromArgb(224, 224, 224),
+    };
+
+    string path = @"meter";
+    Directory.CreateDirectory(path);
+    using (Bitmap bmp = Divergence.Draw(text, divergenceArgs))
+        bmp.Save(path + "/" + text + ".png");
+}
+for (int i=0; i<10; i++)
+{
+    make_divergence(i.ToString());
+}
+make_divergence(".");
+make_divergence("-");
+make_divergence(" ");
---
+++ README.md
@@ -0,0 +1,2 @@
+### Script used to create the divergence meter sprites on my website
+#### Credits to: https://github.com/trigger-segfault/TriggersTools.SteinsGate
